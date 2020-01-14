801aa8b545b36d76b0481c06faa47e527659cd94
ettoremessina <et.messina@gmail.com>
2020-01-14 08:39:34 +0100

    fixed pmc2t_fit_1.py and added pmc2t_predict_1.py

--- parametric-curve-on-plane-fitting/examples/example1_1.sh
+++ parametric-curve-on-plane-fitting/examples/example1_1.sh
@@ -7,8 +7,8 @@ FYT="0.1 * t * np.sin(t)"
 
 python ../pmc2t_gen.py --dsout datasets/example1_1_train.csv --xt "$FXT" --yt "$FYT" --rbegin 0 --rend 20.0 --rstep 0.01
 python ../pmc2t_fit_1.py --trainds datasets/example1_1_train.csv --modelout models/example1_1 \
-  --hlayers 200 300 200 --hactivation sigmoid tanh sigmoid \
-  --epochs 1000
+  --hlayers 200 300 200 --hactivation 'Sigmoid()' 'Tanh()' 'Sigmoid()' \
+  --epochs 100
 
 python ../pmc2t_gen.py --dsout datasets/example1_1_test.csv  --xt "$FXT" --yt "$FYT" --rbegin 0 --rend 20.0 --rstep 0.0475
 python ../pmc2t_predict_1.py --model models/example1_1 --testds datasets/example1_1_test.csv --predicted predictions/example1_1_pred.csv
