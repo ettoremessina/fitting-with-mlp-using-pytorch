80b8562bcdbddfb022ce17468875e32170a8dd67
ettoremessina <et.messina@gmail.com>
2020-01-16 00:27:38 +0100

    example5 of pmc2t changed

--- parametric-curve-on-plane-fitting/examples/example5.sh
+++ parametric-curve-on-plane-fitting/examples/example5.sh
@@ -7,8 +7,9 @@ FYT="3 * np.sin(t)"
 
 python ../pmc2t_gen.py --dsout datasets/example5_train.csv --xt "$FXT" --yt "$FYT" --rbegin 0 --rend 12.56 --rstep 0.01
 python ../pmc2t_fit.py --trainds datasets/example5_train.csv --modelout models/example5.pth \
-  --hlayers 100 100 --hactivation 'ReLU()' 'ReLU()' \
-  --epochs 500
+  --hlayers 150 150 --hactivation 'ReLU()' 'ReLU()' \
+  --epochs 400 \
+  --optimizer 'Adamax(lr=0.01)'
 
 python ../pmc2t_gen.py --dsout datasets/example5_test.csv --xt "$FXT" --yt "$FYT" --rbegin 0 --rend 12.56 --rstep 0.0475
 python ../pmc2t_predict.py --model models/example5.pth --ds datasets/example5_test.csv --predicted predictions/example5_pred.csv
--- parametric-curve-on-plane-fitting/examples/example5_twin.sh
+++ parametric-curve-on-plane-fitting/examples/example5_twin.sh
@@ -7,8 +7,9 @@ FYT="3 * np.sin(t)"
 
 python ../pmc2t_gen.py --dsout datasets/example5_twin_train.csv --xt "$FXT" --yt "$FYT" --rbegin 0 --rend 12.56 --rstep 0.01
 python ../pmc2t_fit_twin.py --trainds datasets/example5_twin_train.csv --modelout models/example5_twin.pth \
-  --hlayers 100 100 --hactivation 'ReLU()' 'ReLU()' \
-  --epochs 500
+  --hlayers 150 150 --hactivation 'ReLU()' 'ReLU()' \
+  --epochs 400 \
+  --optimizer 'Adamax(lr=0.01)'
 
 python ../pmc2t_gen.py --dsout datasets/example5_twin_test.csv --xt "$FXT" --yt "$FYT" --rbegin 0 --rend 12.56 --rstep 0.0475
 python ../pmc2t_predict_twin.py --model models/example5_twin.pth --ds datasets/example5_twin_test.csv --predicted predictions/example5_twin_pred.csv
