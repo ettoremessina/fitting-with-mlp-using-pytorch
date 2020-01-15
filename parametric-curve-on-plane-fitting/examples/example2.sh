230efd5e418fac9b04c0b766be3cb2a77d5d3548
ettoremessina <et.messina@gmail.com>
2020-01-16 00:03:41 +0100

    pmc2t: suppresssed learning_rate param

--- parametric-curve-on-plane-fitting/examples/example2.sh
+++ parametric-curve-on-plane-fitting/examples/example2.sh
@@ -9,7 +9,7 @@ python ../pmc2t_gen.py --dsout datasets/example2_train.csv --xt "$FXT" --yt "$FY
 python ../pmc2t_fit.py --trainds datasets/example2_train.csv --modelout models/example2.pth \
   --hlayers 200 200 200 --hactivation 'Sigmoid()' 'Sigmoid()' 'Sigmoid()' \
   --epochs 250 \
-  --optimizer 'Adamax()'
+  --optimizer 'Adamax(lr=1.1e-2)'
 
 python ../pmc2t_gen.py --dsout datasets/example2_test.csv  --xt "$FXT" --yt "$FYT" --rbegin 0 --rend 20.0 --rstep 0.0475
 python ../pmc2t_predict.py --model models/example2.pth --ds datasets/example2_test.csv --predicted predictions/example2_pred.csv
--- parametric-curve-on-plane-fitting/examples/example2_twin.sh
+++ parametric-curve-on-plane-fitting/examples/example2_twin.sh
@@ -9,7 +9,7 @@ python ../pmc2t_gen.py --dsout datasets/example2_twin_train.csv --xt "$FXT" --yt
 python ../pmc2t_fit_twin.py --trainds datasets/example2_twin_train.csv --modelout models/example2_twin.pth \
   --hlayers 200 200 200 --hactivation 'Sigmoid()' 'Sigmoid()' 'Sigmoid()' \
   --epochs 250 \
-  --optimizer 'Adamax()'
+  --optimizer 'Adamax(lr=1.1e-2)'
 
 python ../pmc2t_gen.py --dsout datasets/example2_twin_test.csv  --xt "$FXT" --yt "$FYT" --rbegin 0 --rend 20.0 --rstep 0.0475
 python ../pmc2t_predict_twin.py --model models/example2_twin.pth --ds datasets/example2_twin_test.csv --predicted predictions/example2_twin_pred.csv
